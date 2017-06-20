cc7d3933017d6387b1cdc980fddea86e65eda2d0
Pau Iglesias <14270729b@iespoblenou.org>
2017-06-20 15:31:48 +0200

    - Objecte Summary adaptat per poder ser utilitzar per Realm -Afegit un item de menu al DetailsActivity que servirà per guardar els resultats o edicions de les imatges o pdf parsejades - Modificat el DetailsActivityFragment per tal de procesar les uris pasades des dels intents amb la fi de  convertirles en fitxers i utilitzar les noves clases creades de l'api .

--- app/src/main/java/com/pruebascongit/pau/tabs/DetailsActivityFragment.java
+++ app/src/main/java/com/pruebascongit/pau/tabs/DetailsActivityFragment.java
@@ -15,14 +15,22 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.gson.JsonObject;
-import com.pruebascongit.pau.tabs.Api.OCRapi;
+import com.pruebascongit.pau.tabs.Api.OCRapiForParseFile;
+import com.pruebascongit.pau.tabs.Api.OCRapiForParseUrl;
 import com.pruebascongit.pau.tabs.GsonUtils.JsonResponseToSummaryPojo;
 import com.pruebascongit.pau.tabs.Pojos.Summary;
+import com.pruebascongit.pau.tabs.Utils.getFileNameFrom;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import static com.bumptech.glide.request.RequestOptions.fitCenterTransform;
 
 
@@ -33,6 +41,7 @@ public class DetailsActivityFragment extends Fragment {
 
 
     private String fileSrc;
+    private String filename;
     private View view;
     private TextView fileSource;
     private EditText textResult;
@@ -67,6 +76,7 @@ public class DetailsActivityFragment extends Fragment {
 
         if(dataCaught!= null) {
 
+            //Si es un intent extern intent.getAction() tindrá el vañor "ACTION_SEND" o "ACTION_VIEW"
             if (dataCaught.getAction() != null) {
                 externalIntentFilter(dataCaught);
             } else {
@@ -78,6 +88,11 @@ public class DetailsActivityFragment extends Fragment {
         return view;
     }
 
+    /**
+     * Filtramos el intent externo (intent-filter) para saber si se trata de una imagen o pdf en
+     * una url o uri.
+     * @param intentFilter
+     */
     private void externalIntentFilter(Intent intentFilter){
 
         String action = intentFilter.getAction();
@@ -98,12 +113,12 @@ public class DetailsActivityFragment extends Fragment {
                     case ".bmp":
                     case ".gif":
                         fileSrc = sharedText;
-                        serviceStarter("capture");
+                        serviceStarter("urlImage");
                         break;
 
                     case ".pdf":
                         fileSrc = sharedText;
-                        serviceStarter("pdf");
+                        serviceStarter("urlPdf");
                         break;
 
                     default:
@@ -111,20 +126,29 @@ public class DetailsActivityFragment extends Fragment {
                 }
 
             }
-
+            filename = getFileNameFrom.Url(fileSrc);
+            //Pdf file from local o external storage i.e. uri
         } else if (Intent.ACTION_VIEW.equals(action) && type != null) {
 
             Uri fileUri = intentFilter.getData();
 
             if (fileUri != null) {
 
-                fileSrc = intentFilter.getData().getPath();
+                fileSrc = fileUri.toString();
+
+                filename = getFileNameFrom.Uri(getContext(), fileSrc);
+
                 serviceStarter("pdf");
             }
         }
 
     }
 
+    /**
+     * Filtramos el intent que viene del app para saber si se trata de una foto o pdf a subir para
+     * parsear o de un parseado almacenado en la bd que queremos ver o editar.
+     * @param dataCaught
+     */
     private void  explicitIntent (Intent dataCaught){
 
 
@@ -134,16 +158,32 @@ public class DetailsActivityFragment extends Fragment {
             fileSrc = dataCaught.getStringExtra("preview");
         else
             summaryFromListView = (Summary) dataCaught.getSerializableExtra("preview");
+
         serviceStarter(mode);
 
     }
 
+    /**
+     *
+     * @param mode
+     */
     private void serviceStarter(String mode) {
 
         switch (mode) {
 
+                case "urlImage":
+                    loadImage(false);
+                    startDownloadingUrlParsingResult();
+                    break;
+
+                case "urlPdf":
+                    setPdfDrawable();
+                    startDownloadingUrlParsingResult();
+                    break;
+
                 case "capture":
-                    loadImage();
+                    loadImage(true);
+                    //filename = getFileNameFrom.Uri(getContext(), fileSrc);
                     checkFileSize();
                     break;
 
@@ -166,38 +206,46 @@ public class DetailsActivityFragment extends Fragment {
         imageFile.setImageResource(R.drawable.ic_pdf);
     }
 
-    private void loadImage() {
-        System.out.println("File source -> "+fileSrc);
-        Glide
-                .with(getContext())
-                .load(fileSrc)
-                .apply(fitCenterTransform())
-                .into(imageFile);
+    private void loadImage(boolean isUri) {
+        System.out.println("Uri source -> "+fileSrc);
+
+        if(isUri)
+            Glide
+                    .with(getContext())
+                    .load(Uri.parse(fileSrc))
+                    .apply(fitCenterTransform())
+                    .into(imageFile);
+        else
+            Glide
+                    .with(getContext())
+                    .load(fileSrc)
+                    .apply(fitCenterTransform())
+                    .into(imageFile);
     }
 
     private void checkFileSize(){
 
-        long imageSize =( ((new File(fileSrc)).length())/1024);
-        System.out.println("IMAGESIZE -> "+imageSize);
-        if(imageSize <= 1024 ) {
-            fileSource.setText(fileSrc);
-            startDownloadImageParsingResult();
-        }else {
-            textResult.setText(R.string.file_too_big);
+        File fileToUpload = convertUriToFile();
+
+        System.out.println("CheckFile Size  -> "+filename);
+
+        if(fileToUpload != null && fileToUpload.length()/1024 <= 1024){
+
+            startDownloadingFileParsingResult(fileToUpload);
         }
+
     }
 
-
-    private void startDownloadImageParsingResult() {
-
+    private void startDownloadingFileParsingResult(File fileToUpload) {
         final String language;
         SharedPreferences preferencias = PreferenceManager.getDefaultSharedPreferences(getContext());
 
-        language = preferencias.getString("lang", "esp");
+        language = preferencias.getString("lang", "eng");
 
-        System.out.println("LANGUAGE -> "+language);
+        System.out.println("LANGUAGE -> "+language+"; AND Filename -> "+fileToUpload.getName());
 
-        OCRapi.Call(getContext(), fileSrc, language, new OCRapi.call() {
+        OCRapiForParseFile.Call(getContext(), fileToUpload, language, new OCRapiForParseFile.call()
+        {
 
             @Override
             public void onCompleted(Exception e, JsonObject response) {
@@ -208,19 +256,118 @@ public class DetailsActivityFragment extends Fragment {
                         textResult.setText(e.toString()+"\n"+summary.getError());
                         //FAIL!! Show TOAST!
                     } else {
-                        summaryFromListView = summary;
+                        //summaryFromListView = summary;
                         textResult.setText(summary.getContent());
+                        System.out.println("Summary "+summary.getFileSrc());
+                        fileSource.setText(fileSrc);
                     }
                 }else
                     textResult.setText(R.string.unknow_fail);
 
             }
         });
+
+        fileToUpload.deleteOnExit();
+    }
+
+    private void startDownloadingUrlParsingResult() {
+
+        final String language;
+        SharedPreferences preferencias = PreferenceManager.getDefaultSharedPreferences(getContext());
+
+        language = preferencias.getString("lang", "eng");
+
+        System.out.println("LANGUAGE -> "+language);
+
+        OCRapiForParseUrl.Call(getContext(), fileSrc, language, new OCRapiForParseUrl.call() {
+
+            @Override
+            public void onCompleted(Exception e, JsonObject response) {
+
+                if (response != null) {
+                    Summary summary = JsonResponseToSummaryPojo.parseJson(response,fileSrc,language);
+                    if (e != null) {
+                        textResult.setText(e.toString()+"\n"+summary.getError());
+                        //FAIL!! Show TOAST!
+                    } else {
+                        //summaryFromListView = summary;
+                        textResult.setText(summary.getContent());
+                        fileSource.setText(fileSrc);
+                    }
+                }else
+                    textResult.setText(R.string.unknow_fail);
+
+            }
+        });
+
+
     }
 
     private void fillLayoutViews(){
         textResult.setText(summaryFromListView.getContent());
-        fileSource.setText(summaryFromListView.getFileName());
+        fileSource.setText(summaryFromListView.getFileSrc());
+    }
+
+    public File convertUriToFile() {
+
+        if (!fileSrc.isEmpty()) {
+            Uri uri = Uri.parse(fileSrc);
+
+            File f = createTempFile();
+
+            writeUriContentToFile(f, uri);
+
+            Toast.makeText(getContext(), "Tamaño del archivo -> " +f.getName()+" "+ f.length(), Toast.LENGTH_LONG).show();
+
+            return f;
+
+        } else{
+            Toast.makeText(getContext(), "La uri no se ha  pillado -> " + fileSrc, Toast.LENGTH_LONG).show();
+        }
+
+        return null;
+
+    }
+
+
+    private void  writeUriContentToFile(File cacheFile, Uri uri){
+
+        InputStream input = null;
+        FileOutputStream outputStream = null;
+        try {
+            input =  getActivity().getContentResolver().openInputStream(uri);
+            outputStream= new FileOutputStream(cacheFile, false);
+            int read = 0;
+            byte[] bytes = new byte[1024];
+            while ((read = input.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+            outputStream.flush();
+            outputStream.getFD().sync();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private File createTempFile(){
+
+        File f = new File(getContext().getCacheDir(),"cacheData");
+
+        f.mkdirs();
+
+        filename = getFileNameFrom.Uri(getContext(),fileSrc);
+
+        System.out.println("Whats my madafuking filename -> "+filename);
+
+        File cacheFile = new File(f,filename);
+
+        cacheFile.deleteOnExit();
+
+        System.out.println("Whats my madafuking name -> "+cacheFile.getName());
+
+        return cacheFile;
     }
 
 
@@ -249,6 +396,21 @@ public class DetailsActivityFragment extends Fragment {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+/*
+    private void checkFileSize(){
+
+        long imageSize =( ((new File(fileSrc)).length())/1024);
+        System.out.println("IMAGESIZE -> "+imageSize);
+        if(imageSize <= 1024 ) {
+            fileSource.setText(fileSrc);
+            startDownloadImageParsingResult();
+        }else {
+            textResult.setText(R.string.file_too_big);
+        }
+    }
+*/
 
 }
 /*
@@ -309,4 +471,90 @@ public void onCompleted(Exception e, Response<JsonObject> response) {
             }
         }
 
- */
+
+
+
+
+         private void startDownloadingFileParsingResult(File fileToUpload) {
+
+        final String language;
+        SharedPreferences preferencias = PreferenceManager.getDefaultSharedPreferences(getContext());
+
+        language = preferencias.getString("lang", "eng");
+
+        System.out.println("LANGUAGE -> "+language);
+
+        OCRapiForParseFile.Call(getContext(), fileToUpload, language, new OCRapi.call() {
+
+            @Override
+            public void onCompleted(Exception e, JsonObject response) {
+
+                if (response != null) {
+                    Summary summary = JsonResponseToSummaryPojo.parseJson(response,fileSrc,language);
+                    if (e != null) {
+                        textResult.setText(e.toString()+"\n"+summary.getError());
+                        //FAIL!! Show TOAST!
+                    } else {
+                        //summaryFromListView = summary;
+                        textResult.setText(summary.getContent());
+                        fileSource.setText(fileSrc);
+                    }
+                }else
+                    textResult.setText(R.string.unknow_fail);
+
+            }
+        });
+    }
+
+    private void startDownloadingUrlParsingResult() {
+
+        final String language;
+        SharedPreferences preferencias = PreferenceManager.getDefaultSharedPreferences(getContext());
+
+        language = preferencias.getString("lang", "eng");
+
+        System.out.println("LANGUAGE -> "+language);
+
+        OCRapiForParseUrl.Call(getContext(), fileSrc, language, new OCRapi.call() {
+
+            @Override
+            public void onCompleted(Exception e, JsonObject response) {
+
+                if (response != null) {
+                    Summary summary = JsonResponseToSummaryPojo.parseJson(response,fileSrc,language);
+                    if (e != null) {
+                        textResult.setText(e.toString()+"\n"+summary.getError());
+                        //FAIL!! Show TOAST!
+                    } else {
+                        //summaryFromListView = summary;
+                        textResult.setText(summary.getContent());
+                        fileSource.setText(fileSrc);
+                    }
+                }else
+                    textResult.setText(R.string.unknow_fail);
+
+            }
+        });
+
+    }
+
+
+
+          }else {
+            try {
+                cacheFile = File.createTempFile(
+                        filename// filename.substring(0,filename.lastIndexOf(".")
+                        ,filename.substring(filename.lastIndexOf(".")+1)
+                        ,f);
+cacheFile =
+        cacheFile.renameTo()
+        cacheFile.deleteOnExit();
+        return  cacheFile;
+        } catch (IOException e) {
+        e.printStackTrace();
+        }
+        }
+
+        return null;
+
+*/
--- app/src/main/java/com/pruebascongit/pau/tabs/Pojos/Summary.java
+++ app/src/main/java/com/pruebascongit/pau/tabs/Pojos/Summary.java
@@ -1,18 +1,16 @@
 package com.pruebascongit.pau.tabs.Pojos;
 
-import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-/**
- * Created by pau on 7/06/17.
- */
+import io.realm.RealmObject;
 
-public class Summary implements Serializable{
+
+public class Summary extends RealmObject{
 
     private String id;
-    private String fileName;
+    private String fileSrc;
     private String error;
     private String lang;
     private String date;
@@ -24,25 +22,25 @@ public class Summary implements Serializable{
                 .format(new Date())+" ]";
     }
 
-    public Summary(String fileName, String error, String lang, String date, String content) {
+    public Summary(String fileSrc, String error, String lang, String date, String content) {
 
         id = "Object creation time [ "+new
                 SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS", new Locale("es","ES"))
                 .format(new Date())+" ]";
 
-        this.fileName = fileName;
+        this.fileSrc = fileSrc;
         this.error = error;
         this.lang = lang;
         this.date = date;
         this.content = content;
     }
 
-    public String getFileName() {
-        return fileName;
+    public String getFileSrc() {
+        return fileSrc;
     }
 
     public void setfileName(String fileName) {
-        this.fileName= fileName;
+        this.fileSrc = fileName;
     }
 
     public String getError() {
@@ -86,7 +84,7 @@ public class Summary implements Serializable{
     public String toString() {
         return "Summary{" +
                 "id='" + id + '\'' +
-                "fileName='" + fileName + '\'' +
+                "fileSrc='" + fileSrc + '\'' +
                 ", error='" + error + '\'' +
                 ", lang='" + lang + '\'' +
                 ", date='" + date + '\'' +
